e5e8cffc4ab45814f3b121f372104177ac3a551f
Augustus0916 <lijunhao0916@gmail.com>
2021-10-19 12:58:41 +0800

    spring+hibernate+consul+gateway+swagger

---
+++ demo-common/demo-swagger/src/main/java/ljh/augustus/demoswagger/configure/SwaggerConfigure.java
@@ -0,0 +1,42 @@
+package ljh.augustus.demoswagger.configure;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfigure {
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("API接口文档")
+                .description("demo文档")
+                .version("1.0.0")
+                .build();
+    }
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ljh.augustus"))
+                .apis(RequestHandlerSelectors.any()) //这里写的是API接口所在的包位置
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return new UiConfiguration(null, "list", "alpha", "schema",
+                UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS, false, true, 60000L);
+    }
+
+}
